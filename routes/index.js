7b63d885cb29c03297aac3ae1d389201c8934982
wndudtjd <dudtjdwn1575@naver.com>
2023-03-29 21:01:56 +0900

    수정본 업데이트

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ const roomsRouter = require('./rooms.route')
 const socialRouter = require('./social.route')
 // const re_commentsRouter = require("./re_comments.route"); // 추후에 기능 구현 할 수도 있음
 // const commentsRouter = require("./comments.route"); // 추후에 기능 구현 할 수도 있음
+
 router.use('/social', socialRouter)
 router.use('/user/signup', signupRouter)
 router.use('/user/login', loginRouter)
